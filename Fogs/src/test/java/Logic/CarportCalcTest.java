d0553c248b4152961ba2a235d748226de80ecd3e
vince <vince@DESKTOP-9IJNJ2M>
2019-05-21 16:51:30 +0200

    Added unitTest to LogicFacadeMOCK added equals method in carport

--- Fogs/src/test/java/Logic/CarportCalcTest.java
+++ Fogs/src/test/java/Logic/CarportCalcTest.java
@@ -66,7 +66,80 @@ public class CarportCalcTest {
         expected.setLength(600);
         expected.setTotalPrice(710 * 14);
         expected.setUnit("Stk");
-        Material actual = calc.rafts(780,600);
+        Material actual = calc.rafts(780, 600);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void raftDistanceTest() throws SQLException, ClassNotFoundException {
+        double expected = 55;
+        double actual = calc.raftDistance(780, 600);
+        assertThat(expected, is(actual));
+    }
+
+    @Test
+    public void portFittingsTest() throws SQLException, ClassNotFoundException {
+        Material expected = new Material("Vinkelbeslag 35 mm", 200);
+        int pamount = 6;
+        int ramount = 2 * 14;
+        expected.setAmount(pamount + ramount);
+        expected.setTotalPrice(200 * (pamount + ramount));
+        expected.setUnit("Stk");
+        Material actual = calc.portFittings(780, 600);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void frameLengthTest() throws SQLException, ClassNotFoundException {
+        Material expected = new Material("19x100 mm. trykimp. Brædt", 410);
+        expected.setLength(780);
+        expected.setAmount(2);
+        expected.setTotalPrice(410 * 2);
+        expected.setUnit("Stk");
+        Material actual = calc.frameLength(780, 600);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void frameWidthTest() throws SQLException, ClassNotFoundException {
+        Material expected = new Material("19x100 mm. trykimp. Brædt", 410);
+        expected.setLength(600);
+        expected.setAmount(2);
+        expected.setTotalPrice(410 * 2);
+        expected.setUnit("Stk");
+        Material actual = calc.frameWidth(780, 600);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void screwFrameTest() throws SQLException, ClassNotFoundException {
+        Material expected = new Material("4,5 x 50 mm. skruer 300 stk.", 350);
+        expected.setAmount(1);
+        expected.setTotalPrice(350);
+        expected.setUnit("Pakke");
+        Material actual = calc.screwFrame(780, 600);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void plastmoShortTest() throws SQLException, ClassNotFoundException {
+        Material expected = new Material("Plastmo Exolite blåtonet", 905);
+
+        expected.setAmount((int) Math.ceil((780 * 600 / 100) * 0.46 / 360));
+        expected.setTotalPrice(905 * expected.getAmount());
+        expected.setUnit("Stk");
+        Material actual = calc.plastmoShort(780, 600);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void plastmoLongTest() throws SQLException, ClassNotFoundException {
+        Material expected = new Material("Plastmo Exolite blåtonet.", 390);
+
+        expected.setAmount((int) Math.ceil((780 * 600 / 100) * 0.77 / 600));
+        expected.setTotalPrice(390 * expected.getAmount());
+        expected.setUnit("Stk");
+        Material actual = calc.plastmoLong(780, 600);
         assertEquals(expected, actual);
     }
 
